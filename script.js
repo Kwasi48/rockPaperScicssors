6f8858380ec0921e8c31e96c0231635c4d37ae49
Kwasi48 <abumichaelobeng@gmail.com>
2026-09-17 23:03:49 +0000

    added getHumanChoice

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice(){
+    choice = parseInt(prompt("Enter 0 for rock, 1 for paper and 2 for scissors"));
+    if (choice === 0){
+        return rock;
+    } else if (choice === 1){
+        return paper;
+    } else if (choice === 2)
+        {
+        return scissors;
+    }
+}
+
+console.log(getHumanChoice());
