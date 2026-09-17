39f50eb8832e35d5578a8a0b676ba3b40b9351af
Kwasi48 <abumichaelobeng@gmail.com>
2026-09-17 22:50:12 +0000

    added getComputerChoice

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-
 console.log("Hello World")
+
+function getComputerChoice(){
+    rock = "rock";
+    paper = "paper";
+    scissors = "scissors"; 
+
+    max  = 3;
+    value = Math.floor(Math.random() * max);
+    if (value === 0) {
+        return rock;
+    } else if (value === 1){
+        return paper;
+    } else {
+        return scissors;
+    }
+}
+
+console.log(getComputerChoice())
